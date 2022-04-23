01064fdc4f9f320852a1e52f7a3ce46a1d3bb40f
eloco <exia@isimarkets.com>
2022-04-23 14:02:03 +0800

    init heroku

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+ARG FUNCTION_DIR="/function"
+
+FROM mcr.microsoft.com/playwright/python:latest as build-image
+ARG FUNCTION_DIR
+
+# Create function directory
+RUN mkdir -p ${FUNCTION_DIR}
+
+# Copy function code and requirements
+COPY app/* ${FUNCTION_DIR}/
+
+# Install the runtime interface client
+RUN python -m pip install -r ${FUNCTION_DIR}/requirements.txt \
+        --target ${FUNCTION_DIR} \
+        --no-cache-dir 
+
+# Multi-stage build: grab a fresh copy of the base image
+FROM mcr.microsoft.com/playwright/python:latest
+
+ARG FUNCTION_DIR
+# Set working directory to function root directory
+WORKDIR ${FUNCTION_DIR}
+
+# Copy in the build image dependencies
+COPY --from=build-image ${FUNCTION_DIR} ${FUNCTION_DIR}
+
+ENV PORT=80
+EXPOSE ${PORT}
+
+CMD gunicorn main:app
---
+++ app/main.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#Import the flask module
+from wrapt_timeout_decorator import *
+from flask import Flask, request
+from playwright.sync_api import sync_playwright
+from playwright_stealth import stealth_sync
+import base64
+import os,sys
+import datetime,time,random
+import httpx,requests
+import traceback
+
+app = Flask(__name__)
+
+result = 'hello world'
+
+@app.route('/')
+def hello_world():
+    statement = 'Hello World!'
+    return statement
+
+
+"""
+param = {
+        run:"result='test'"; # base64 or normal diy code,             
+        browser:"chromium";  # browser name,
+        device:"iPhone X";   # device for webkit
+        stealth:true;        # if stealth mode
+        }
+"""
+
+@timeout(60*10) # 10 minutes
+@app.route('/post', methods=['POST'])
+def playwright():
+    status_code = 200
+    try:
+        with sync_playwright() as playwright:
+            global result
+            if browser_name:=request.form.get('browser', default=False):
+                device_name=request.form.get('device', default='')
+                if   browser_name.lower() == "chromium":
+                    browser = playwright.chromium.launch(headless=True)
+                    context = browser.new_context()
+                elif browser_name.lower() == "firefox":
+                    browser = playwright.firefox.launch(headless=True)
+                    context = browser.new_context()
+                else:
+                    browser = playwright.webkit.launch(headless=True)
+
+                    if device_name.lower() == "random":
+                       device_name = random.choice(list(playwright.devices.keys()))
+                    else:
+                        for d in playwright.devices.keys():
+                            if device_name.lower() == d.lower():
+                                device_name = d
+                                break
+                        if device_name not in playwright.devices.keys():
+                            device_name = "Desktop Firefox"  # use default
+                    print(f"webkit using [{device_name}]")
+                    device      = playwright.devices[device_name]
+                    context     = browser.new_context(**device,)
+                page = context.new_page()
+                if stealth:=request.form.get('stealth', default=False):
+                    print(stealth)
+                    print("page is stealth!")
+                    stealth_sync(page)
+
+            run="global result;"
+            if command:=request.form.get('run', default=False):
+                try:
+                    run+=base64.b64decode(command).decode('utf-8')
+                except:
+                    run+=command
+            print(run)
+            exec(run)
+    except Exception as e:
+        result=str(e)
+        traceback.print_exc()
+        status_code = 500
+
+    return {
+        'code': status_code,
+        'result': result,
+    }
+
+#Calls the run method, runs the app on port 80
+app.run(host='0.0.0.0', port='80')
